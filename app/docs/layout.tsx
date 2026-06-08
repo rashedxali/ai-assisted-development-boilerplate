e8b8dce45d36c2068311c3c2ab7154d297c750ee
Rashed <tools.rashed@gmail.com>
2026-06-08 23:31:04 +0600

    feat: integrate fumadocs for documentation and enhance project structure
    - Updated `next.config.ts` to include MDX support using `fumadocs-mdx`.
    - Added new configuration file `source.config.ts` for documentation structure.
    - Introduced new components and layouts for documentation pages.
    - Enhanced CSS imports in `globals.css` for improved styling.
    - Created initial documentation content in `content/docs` directory.
    - Updated `package.json` to include new dependencies for fumadocs.

---
+++ app/docs/layout.tsx
@@ -0,0 +1,18 @@
+import { source } from "@/lib/source";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { RootProvider } from "fumadocs-ui/provider/next";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <RootProvider>
+      <DocsLayout tree={source.getPageTree()} sidebar={{ className: "w-fit" }}>
+        {children}
+      </DocsLayout>
+    </RootProvider>
+  );
+}
